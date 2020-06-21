1330da0a56e21b44888dc15555c4440479196042
Afonasev Evgeniy <ea.afonasev@gmail.com>
2020-06-22 03:59:57 +0500

    Bump version: 1.0.0 → 1.1.0

--- context_logging/__init__.py
+++ context_logging/__init__.py
@@ -4,4 +4,4 @@ from contextvars_executor import ContextVarExecutor
 from .context import Context, current_context, root_context
 from .log_record import setup_log_record
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
