439428feb3f3b2a31a1983181dfc7509ed266f32
Afonasev Evgeniy <ea.afonasev@gmail.com>
2020-06-22 03:54:44 +0500

    fix context multithreading/multitasking leaks

--- context_logging/__init__.py
+++ context_logging/__init__.py
@@ -1,4 +1,6 @@
 # flake8: noqaF401
+from contextvars_executor import ContextVarExecutor
+
 from .context import Context, current_context, root_context
 from .log_record import setup_log_record
 
--- context_logging/utils.py
+++ context_logging/utils.py
@@ -1,5 +1,28 @@
+# pylint:disable=not-context-manager
+from contextlib import ContextDecorator
 from datetime import timedelta
-from inspect import getframeinfo, stack
+from functools import wraps
+from inspect import getframeinfo, iscoroutinefunction, stack
+from typing import Any, Callable
+
+
+class SyncAsyncContextDecorator(ContextDecorator):
+    def __call__(self, func: Callable[..., Any]) -> Callable[..., Any]:
+        if iscoroutinefunction(func):
+
+            @wraps(func)
+            async def async_inner(*args: Any, **kwds: Any) -> Any:
+                with self._recreate_cm():  # type: ignore
+                    return await func(*args, **kwds)
+
+            return async_inner
+
+        @wraps(func)
+        def sync_inner(*args: Any, **kwds: Any) -> Any:
+            with self._recreate_cm():  # type: ignore
+                return func(*args, **kwds)
+
+        return sync_inner
 
 
 def context_name_with_code_path() -> str:
